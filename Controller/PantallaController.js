82e3aecf30859472ca57e533b6c4da7f612a2a9a
Michulada Rosero <Michulada Rosero>
2017-04-23 21:40:51 -0500

    funcional forma

--- Controller/PantallaController.js
+++ Controller/PantallaController.js
@@ -6,7 +6,7 @@ angular.module('app',[])
         var mostarEditar= false;
         var correctSave=false;
         var  border= 'border-radius';
-        $scope.option1;
+        $scope.options;
         $scope.colorRosado='#de5fcf';
         $scope.colorVerde='#1adebd';
         $scope.colorCeleste='#2ea1de';
@@ -14,7 +14,7 @@ angular.module('app',[])
         $scope.colorRojo='#de213f';
         $scope.colorAmarrillo='#deba1e';
         $scope.colorAzul='#170ede';
-        $scope.cuadrado;
+        $scope.cuadrado='width: 100px height: 100px';
         $scope.circulo;
         $scope.rectangulo;
         $scope.ovalo;
@@ -39,23 +39,41 @@ angular.module('app',[])
         $scope.cambioColor=function (color) {
             console.log(color);
             $scope.miColor. backgroundColor=color;
-        }
+        };
         $scope.miColor={
             backgroundColor:'#1adebd'
-        }
+        };
         $scope.cambioForma=function (forma) {
             console.log(forma)
-            $scope.cuadrado.border='2px'
+            if(forma=='ovalo') {
+                $scope.miForma = document.getElementById("menuPrincipal").style.width = "80px";
+                $scope.miForma = document.getElementById("menuPrincipal").style.height = "40px";
+                $scope.miForma = document.getElementById("menuUno").style.width = "80px";
+                $scope.miForma = document.getElementById("menuUno").style.height = "40px";
+                $scope.miForma = document.getElementById("menuDos").style.width = "80px";
+                $scope.miForma = document.getElementById("menuDos").style.height = "40px";
+                $scope.miForma = document.getElementById("menuTres").style.width = "80px";
+                $scope.miForma = document.getElementById("menuTres").style.height = "40px";
+            }
+            if(forma=='circulo') {
+                $scope.miForma = document.getElementById("menuPrincipal").style.width = "70px";
+                $scope.miForma = document.getElementById("menuPrincipal").style.height = "70px";
+                $scope.miForma = document.getElementById("menuUno").style.width = "70px";
+                $scope.miForma = document.getElementById("menuUno").style.height = "70px";
+                $scope.miForma = document.getElementById("menuDos").style.width = "70px";
+                $scope.miForma = document.getElementById("menuDos").style.height = "70px";
+                $scope.miForma = document.getElementById("menuTres").style.width = "70px";
+                $scope.miForma = document.getElementById("menuTres").style.height = "70px";
+            }
+        };
+        $scope.miForma={
+            width: '90px',
+            height: '90px',
+            border: '3px solid #555'
         }
-
-        $scope.cuadrado={
-            border: '0px'
-
-    }
-
-       // $scope.material-button-toggle={
-       //  width: 90px;
-       //  height: 90,px;
-       //  margin: 0,auto;
-       //  }
+        // $scope.material-button-toggle={
+        //  width: 90px;
+        //  height: 90,px;
+        //  margin: 0,auto;
+        //  }
     });
